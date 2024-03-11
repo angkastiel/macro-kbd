2988d769228d346c8bc0213d684a6aaa6fd61c85
Vitaly Ruda <rudavitalij@yandex.ru>
2024-03-11 19:56:23 +0300

    Create btnopt(rev2).py

---
+++ btnopt(rev2).py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: kastiel
+"""
+
+import board
+
+buttons_pins = [board.GP4, board.GP9, board.GP15, board.GP22, board.GP19, board.GP16]
+dbg_button_pin = board.GP1
